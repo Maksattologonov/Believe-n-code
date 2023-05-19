61b4eb0922fbfd7ce7f80f54ad9d21b8ac1a133e
Maksat <maksattologonovn@gmail.com>
2023-05-19 17:42:59 +0600

    static add

--- main/main/settings.py
+++ main/main/settings.py
@@ -118,8 +118,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # пустая папка, сюда будет собирать статику collectstatic
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+
+    os.path.join(BASE_DIR, 'staticfiles'),)
+
+STATICFILES_FINDERS = (
+
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
