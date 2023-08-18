7d128b88cc4e4461c4fa078f948af903845360d8
makstt <maksattologonovn@gmail.com>
2023-08-18 23:39:23 +0600

    add media files settings

--- main/main/urls.py
+++ main/main/urls.py
@@ -1,12 +1,15 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('payment_app.urls')),
     path('', include('telegram_app.urls'))
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 admin.site.site_header = "Администрирование Believe'n'Code"
